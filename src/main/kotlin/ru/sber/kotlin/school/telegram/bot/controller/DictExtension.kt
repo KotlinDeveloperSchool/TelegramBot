a42268a57b209ea4cc24201ad334bdee92e3ef4e
Korshik <dakorshik@sberbank.ru>
2023-02-21 13:39:19 +0300

    Clear comments

--- src/main/kotlin/ru/sber/kotlin/school/telegram/bot/controller/DictExtension.kt
+++ src/main/kotlin/ru/sber/kotlin/school/telegram/bot/controller/DictExtension.kt
@@ -1,12 +1,7 @@
 package ru.sber.kotlin.school.telegram.bot.controller
 
-import org.telegram.abilitybots.api.bot.BaseAbilityBot
 import org.telegram.abilitybots.api.objects.Reply
-import org.telegram.abilitybots.api.sender.MessageSender
 import org.telegram.abilitybots.api.util.AbilityExtension
-import org.telegram.abilitybots.api.util.AbilityUtils
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import org.telegram.telegrambots.meta.api.objects.Update
 import ru.sber.kotlin.school.telegram.bot.service.DictionaryMenuService
 import ru.sber.kotlin.school.telegram.bot.service.DictionaryService
 import ru.sber.kotlin.school.telegram.bot.util.CustomSender
@@ -18,9 +13,7 @@ class DictExtension(
     private val customSender: CustomSender,
     private val predicates: Predicates,
     private val dictionaryMenuService: DictionaryMenuService,
-    private val dictionaryService:
-    DictionaryService,
-    private val sender: MessageSender
+    private val dictionaryService: DictionaryService
 ) : AbilityExtension {
 
     fun dictMenu(): Reply = Reply.of(
@@ -67,33 +60,4 @@ class DictExtension(
         predicates.checkState(State.Deleting)
             .and(predicates.startsWithText("Вы выбрали словарь:"))
     )
-
-//    fun toMainMenu(): Reply = Reply.of(
-//
-//    )
-
-
-    /**
-     * В дальнейшем для перехода из меню по прочим кнопкам
-     */
-
-    fun inlineReplyTemporary(): Reply {
-        val action: (BaseAbilityBot, Update) -> Unit = { _, upd ->
-            //временная реализация
-            sender.execute(
-                SendMessage(
-                    AbilityUtils.getChatId(upd).toString(),
-                    "Временно, далее здесь методы перехода в меню или указания на создание темы нового словаря " +
-                            "(можно ввод пользователем темы сделать через inlinequery как в примере с fails - " +
-                            "вываливается fails (переименованный типо после двоеточия введите тему словаря: ) " +
-                            "и пользователь вводит тему и и направляет весь этот кусок в чат)"
-                )
-            )
-        }
-        return Reply.of(action, isInlineQueryTemporary())
-    }
-
-    private fun isInlineQueryTemporary(): (Update) -> Boolean = { upd: Update ->
-        upd.hasCallbackQuery() && upd.callbackQuery.data == "temporary"
-    }
 }
